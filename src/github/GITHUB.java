5a7f0a08ac7222c74e01d1694291ab142b8e5d8d
usuario <usuario@192.168.135.83>
2025-08-02 17:27:12 -0600

    "Cambios 03-08-2025"

--- src/github/GITHUB.java
+++ src/github/GITHUB.java
@@ -15,12 +15,14 @@ public class GITHUB {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String nombre,edad;
+        String nombre,edad,telefono;
        Scanner lector=new Scanner(System. in);
         System.out.println("Ingrese su nombre");
         nombre=lector.next();
         System.out.println("Ingrese su edad");
         edad=lector.next();
+        System.out.println("Ingrese su telefono");
+        telefono=lector.next();
     }
     
 }
